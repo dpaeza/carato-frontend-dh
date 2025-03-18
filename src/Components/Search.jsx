63bd850ab0d9d648f801c0a2325570c20af3ab18
dpaeza10 <daniela.paez@inchcape.com>
2025-03-17 19:27:44 -0500

    feat: enhance search component layout and styling with improved brand selection and date range picker

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,34 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import { DateTimePicker } from '@mui/x-date-pickers';
-import { DateRangePicker } from 'rsuite';
-import { InputPicker } from 'rsuite';
+import { DateRangePicker, InputPicker, Button } from 'rsuite';
 import dayjs from 'dayjs';
 import '../Styles/search.css';
-import { Typography, Button, Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import 'rsuite/DateRangePicker/styles/index.css';
+import SearchIcon from '@mui/icons-material/Search';
 import { getBrands } from '../Services/extras';
+import Grid from '@mui/material/Grid2';
 
 export default function Search() {
     // Definir las fechas iniciales: hoy y 4 días después
     const today = dayjs().startOf('day');
     const defaultEndDate = today.add(4, 'day');
 
-    // Estado con valores iniciales
+    // Estados para las fechas
     const [startDate, setStartDate] = useState(today.toDate());
     const [endDate, setEndDate] = useState(defaultEndDate.toDate());
 
-    // Estado para almacenar las marcas
+    // Estados para las marcas
     const [brands, setBrands] = useState([]);
     const [selectedBrand, setSelectedBrand] = useState(null);
 
-    // Efecto para obtener las marcas
+    // Obtener marcas
     useEffect(() => {
         const fetchBrands = async () => {
             try {
-                const response = await getBrands(); // Asume que getBrands() devuelve un array con objetos { id, name }
+                const response = await getBrands(); // Se espera un array con objetos { id, name }
                 const formattedBrands = response.map(brand => ({
                     label: brand.name,
                     value: brand.id
@@ -42,74 +39,130 @@ export default function Search() {
     }, []);
 
     return (
-        <section className='search-component'>
-            <h2>Alquila tu vehiculo</h2>
-            <Box sx={{
+        <Box
+            sx={{
                 display: 'flex',
-                flexDirection: { xs: 'column', sm: 'row' },
+                flexDirection: 'column',
                 gap: 2,
-                alignItems: 'center',
-                justifyContent: 'center',
                 width: '100%',
-            }}>
-                <InputPicker
-                    appearance="default"
-                    placeholder="Selecciona una marca"
-                    data={brands}
-                    value={selectedBrand}
-                    onChange={setSelectedBrand}
-                    size="lg"
-                    style={{ width: 230 }}
-                    searchable={false}
-                    menuStyle={{
-                        padding: '8px', // Espaciado dentro del menú
-                        borderRadius: 8, // Bordes redondeados del menú
-                        boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)', // Sombra suave
-                    }}
-                    renderMenuItem={(label, item) => (
-                        <div
-                            style={{
-                                padding: '8px 12px',
-                                cursor: 'pointer',
-                                borderRadius: 6,
-                                transition: 'background 0.2s ease-in-out',
-                            }}
-                            onMouseEnter={(e) => e.currentTarget.style.background = 'rgba(0, 123, 255, 0.1)'}
-                            onMouseLeave={(e) => e.currentTarget.style.background = 'transparent'}
-                        >
-                            {label}
-                        </div>
-                    )}
-                />
-                <DateRangePicker
-                    appearance="default"
-                    placeholder="Fecha de retiro - Fecha de devolución"
-                    label="Fecha de retiro - Fecha de devolución"
-                    size="lg"
-                    style={{ width: 500 }}
-                    value={[startDate, endDate]}
-                    onChange={(range) => {
-                        // Verifica que range contenga las fechas seleccionadas
-                        if (range) {
-                            setStartDate(range[0]);
-                            setEndDate(range[1]);
-                        }
-                    }}
-                    shouldDisableDate={(date) => dayjs(date).isBefore(dayjs().startOf('day'))}
-                    defaultCalendarValue={[startDate, endDate]}
-                />
-                <Button
-                    className='searchButton'
-                    sx={{
-                        width: { xs: '100%', sm: 'auto' },
-                        backgroundColor: 'var(--lightBlue)',
-                        color: 'white',
-                        px: 2
-                    }}
+                padding: '20px',
+                backgroundColor: 'white',
+                borderRadius: '10px',
+                boxShadow: '0px 0px 10px 0px rgba(0, 0, 0, 0.1)',
+            }}
+        >
+            <Typography
+                variant="h3"
+                component="h2"
+                fontFamily='(var(--openSans))'
+                fontSize={'22px'}
+                fontWeight={700}
+                textAlign={'left'}
+                mb={0.5}
+            >
+                Alquila tu vehículo
+            </Typography>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: { xs: 'column', sm: 'row' },
+                    gap: 2,
+                    alignItems: 'center',
+                    width: '100%',
+                }}
+            >
+                <Grid 
+                    container 
+                    spacing={2}
+                    direction={{ xs: 'column', sm: 'row' }}
+                    alignItems="flex-end"
+                    justifyContent={{ xs: 'center', sm: 'space-between' }}
+                    width={'100%'}
                 >
-                    Buscar
-                </Button>
+                    <Grid size={{xs:12, sm:12, md:3, lg:3 }}>
+                        <label htmlFor="brand-picker" className="input-label">
+                            Marca
+                        </label>
+                        <InputPicker
+                            id="brand-picker"
+                            placeholder="Selecciona una marca"
+                            data={brands}
+                            value={selectedBrand}
+                            onChange={setSelectedBrand}
+                            size="lg"
+                            block
+                            searchable={false}
+                            menuStyle={{
+                                padding: '8px',
+                                borderRadius: 8,
+                                boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+                            }}
+                            renderMenuItem={(label, item) => (
+                                <div
+                                    style={{
+                                        padding: '8px 12px',
+                                        cursor: 'pointer',
+                                        borderRadius: 6,
+                                        transition: 'background 0.2s ease-in-out',
+                                    }}
+                                    onMouseEnter={(e) =>
+                                        (e.currentTarget.style.background = 'rgba(0, 123, 255, 0.1)')
+                                    }
+                                    onMouseLeave={(e) =>
+                                        (e.currentTarget.style.background = 'transparent')
+                                    }
+                                >
+                                    {label}
+                                </div>
+                            )}
+                        />
+                    </Grid>
+                    <Grid size={{xs:12, sm:12, md:7, lg:7 }}>
+                        <label htmlFor="date-range" className="input-label">
+                            Fecha de retiro - Fecha de devolución
+                        </label>
+                        <DateRangePicker
+                            id="date-range"
+                            appearance="default"
+                            placeholder="Selecciona fechas"
+                            size="lg"
+                            className="date-range-picker"
+                            style={{ width: '100%' }}
+                            value={[startDate, endDate]}
+                            onChange={(range) => {
+                                if (range) {
+                                    setStartDate(range[0]);
+                                    setEndDate(range[1]);
+                                }
+                            }}
+                            shouldDisableDate={(date) =>
+                                dayjs(date).isBefore(dayjs().startOf('day'))
+                            }
+                            defaultCalendarValue={[startDate, endDate]}
+                        />
+                    </Grid>
+                    <Grid size={{xs:12, sm:12, md:2}}>
+                        <Button
+                            startIcon={<SearchIcon fontSize='small'/>}
+                            style={{
+                                width: '100%',
+                                fontFamily: '(var(--lato))',
+                                fontWeight: 500,
+                                padding: '10px',
+                                borderRadius: '8px',
+                                fontSize: '15px',
+                                fontWeight: 'bold',
+                                backgroundColor: '#3083FF',
+                                color: 'white',
+                                boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+                            }}
+                        >
+                            Buscar
+                        </Button>
+                    </Grid>
+                </Grid>
             </Box>
-        </section>
+        </Box>
     );
 }
+
