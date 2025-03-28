03822931a379ad4ba4b89c73d10b93bb29fd3da4
dpaeza10 <daniela.paez@inchcape.com>
2025-03-27 19:17:39 -0500

    fix: update WhatsApp message for clarity in FloatButtonWpp component

--- src/Components/FloatButtonWpp.jsx
+++ src/Components/FloatButtonWpp.jsx
@@ -4,7 +4,7 @@ import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 export default function FloatButtonWpp() {
     const phoneNumber = "573022393397"; 
-    const message = encodeURIComponent("¡Hola! Estoy interesado en rentar un vehículo con Carato. ¿Podrían brindarme más información?"); 
+    const message = encodeURIComponent("¡Hola! Estoy interesado en rentar un vehículo con Carato. ¿Me podrían brindar más información?"); 
     const url = `https://wa.me/${phoneNumber}?text=${message}`;
 
     return (
